dd74bda87b5fd71e8664a42bcc826ff9cf00f450
Amira Samir <77174321+AmiraSamirSoliman@users.noreply.github.com>
2021-03-24 17:46:52 +0200

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,13 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath=amirasoliman89/machinelearning:tagname
+#dockerpath=amirasoliman89/machinelearning:tagname
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: "
 docker login --username amirasoliman89 --password amirasoliman89
-docker tag udacitypinzonimage amirasoliman89/machinelearning:tagname
+#docker tag udacitypinzonimage amirasoliman89/machinelearning:tagname
 # Step 3:
 # Push image to a docker repository
 docker push amirasoliman89/machinelearning:tagname
